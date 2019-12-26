fae6ac57f6345282dbae3388c831bdac936d6b6b
Nissen Parianen <n.parianen@gmail.com>
2019-12-26 18:04:47 +0100

    Added legacy apache code

--- app/src/main/java/org/stackwizards/androlib/MainActivity.java
+++ app/src/main/java/org/stackwizards/androlib/MainActivity.java
@@ -8,11 +8,7 @@ import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
+
 
 import org.json.JSONArray;
 import org.json.JSONException;
